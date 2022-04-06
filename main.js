da44b4ba3aea9ca84b4c40f5f54216c93a750ab0
ptbjocom <ptbjocom@gmail.com>
2022-04-06 17:47:25 +0200

    V4

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ class MyHeader extends HTMLElement{
         <nav class="navbar navbar-expand-lg navbar-light">
         <div class="container px-5">
             <a class="navbar-brand" href="#!">
-                <img src="/assets/images/logo" alt="">
+                <img src="assets/images/logo" alt="">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
